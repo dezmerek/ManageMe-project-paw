e6f25b4dcb84bf9c83ed2f4aa28cb0e44a24338d
Dezmerek <mszymanski027@gmail.com>
2023-06-02 19:09:00 +0200

    install bootstrap

--- ManageMe/src/app/app.module.ts
+++ ManageMe/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponentComponent } from './welcome-component/welcome-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponentComponent
   ],
   imports: [
     BrowserModule,
