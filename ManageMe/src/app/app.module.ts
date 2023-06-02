25098bc2ab337db5316dcf13f717be13db8cd9bc
Dezmerek <mszymanski027@gmail.com>
2023-06-02 20:39:48 +0200

    add component login and register

--- ManageMe/src/app/app.module.ts
+++ ManageMe/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomeComponentComponent } from './welcome-component/welcome-component.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeComponentComponent
+    WelcomeComponentComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
