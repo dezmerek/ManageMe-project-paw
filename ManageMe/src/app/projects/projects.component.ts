6bbf40d46ef6906adfbb7978c984e8c00dd0809c
Dezmerek <mszymanski027@gmail.com>
2023-06-03 18:22:30 +0200

    dzialajaca edycja

--- ManageMe/src/app/projects/projects.component.ts
+++ ManageMe/src/app/projects/projects.component.ts
@@ -9,9 +9,72 @@ import { Project } from '../models/project.model';
 export class ProjectsComponent implements OnInit {
   projects: Project[] = [];
   showAddForm: boolean = false;
+  editMode: boolean = false;
+  editedProjectIndex: number | null = null;
+
+  editedProject: Project = {
+    name: '',
+    description: ''
+  };
 
   ngOnInit() {
-    this.projects = [
+    this.loadProjectsFromLocalStorage();
+  }
+
+  toggleAddForm() {
+    this.showAddForm = !this.showAddForm;
+    this.editMode = false; // W przypadku przełączenia na formularz dodawania, wyłączamy tryb edycji
+    this.clearEditedProject();
+  }
+
+  addProject(project: Project) {
+    this.projects.push(project);
+    this.saveProjectsToLocalStorage();
+    this.showAddForm = false;
+  }
+
+  deleteProject(index: number) {
+    this.projects.splice(index, 1);
+    this.saveProjectsToLocalStorage();
+  }
+
+  editProject(project: Project, index: number) {
+    this.editMode = true;
+    this.editedProjectIndex = index;
+    this.editedProject = { ...project }; // Tworzymy kopię projektu do edycji
+    this.showAddForm = true;
+  }
+
+  updateProject() {
+    if (this.editedProjectIndex !== null) {
+      this.projects[this.editedProjectIndex] = { ...this.editedProject };
+      this.saveProjectsToLocalStorage();
+      this.showAddForm = false;
+      this.clearEditedProject();
+    }
+  }
+
+  private saveProjectsToLocalStorage() {
+    localStorage.setItem('projects', JSON.stringify(this.projects));
+  }
+
+  private loadProjectsFromLocalStorage() {
+    const savedProjects = localStorage.getItem('projects');
+    if (savedProjects) {
+      try {
+        this.projects = JSON.parse(savedProjects);
+        console.log('Wczytano projekty z localStorage:', this.projects);
+      } catch (error) {
+        console.error('Błąd parsowania danych projektów z localStorage:', error);
+        this.projects = this.getDefaultProjects(); // Używamy domyślnych danych projektów
+      }
+    } else {
+      this.projects = this.getDefaultProjects(); // Używamy domyślnych danych projektów
+    }
+  }
+
+  private getDefaultProjects(): Project[] {
+    return [
       {
         name: 'Projekt A',
         description: 'Mauris a ex hendrerit, bibendum lectus non, auctor neque. Fusce ut lorem ante. Morbi blandit purus gravida turpis dapibus rutrum.'
@@ -27,21 +90,15 @@ export class ProjectsComponent implements OnInit {
     ];
   }
 
-  toggleAddForm() {
-    this.showAddForm = !this.showAddForm;
-  }
-
-  addProject(project: Project) {
-    this.projects.push(project);
-    this.saveProjectsToLocalStorage(this.projects);
-    this.showAddForm = false;
-  }
-
-  private saveProjectsToLocalStorage(projects: Project[]) {
-    localStorage.setItem('projects', JSON.stringify(projects));
+  private clearEditedProject() {
+    this.editedProjectIndex = null;
+    this.editedProject = {
+      name: '',
+      description: ''
+    };
   }
 
   workOnProject(project: Project) {
-    console.log('Working on project:', project.name);
+    console.log('Pracuję nad projektem:', project.name);
   }
 }
