91b510c435db9037c7e3c57cd8942d6b8e62dd5e
Dezmerek <mszymanski027@gmail.com>
2023-06-04 02:56:29 +0200

    add

---
+++ ManageMe/src/app/models/functionality.model.ts
@@ -0,0 +1,9 @@
+export interface Functionality {
+    id: string;
+    name: string;
+    description: string;
+    priority: string;
+    project: string;
+    owner: string;
+    status: string;
+}
